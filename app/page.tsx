0e1ee0751e4bdaa9c9f93eb618ae9cc8842ddaaf
Yerzhan Saulembekov <yerzhan.saulembekov@gmail.com>
2026-06-26 21:36:44 +0500

    fix: включить Комиссию ИП 6% в расчёт ЧДП BI аналитики

--- app/page.tsx
+++ app/page.tsx
@@ -94,7 +94,7 @@ const IcoNet      = <svg viewBox="0 0 20 20" fill="currentColor" className="w-5 
 const IcoClock    = <svg viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd"/></svg>
 
 function AnalyticsSection({ dds }: { dds: DDSSummary }) {
-  const netBI = dds.openingBalanceBI + dds.totalIncomeBI - dds.totalFotBI - dds.totalTaxBI - dds.totalOverhead
+  const netBI = dds.openingBalanceBI + dds.totalIncomeBI - dds.totalFotBI - dds.totalIpCommissionBI - dds.totalTaxBI - dds.totalOverhead
   const netSENSATA = dds.openingBalanceSENSATA + dds.totalIncomeSENSATA - dds.totalFotSENSATA - dds.totalTaxSENSATA
 
   return (
